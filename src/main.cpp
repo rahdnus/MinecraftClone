c64653aa51ab6bada8e718d9bbda4a624a6d1802
rahdnus <rahdnus119@gmail.com>
2022-04-11 20:03:05 +0530

    widnow set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include<main.hpp>
 #include<stdio.h>
-
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 int main()
 {
-    printf("Hellow world");
+    if(!glfwInit())
+        return -1;
+ 
+   GLFWwindow* window=glfwCreateWindow(500,500,"New Project :(",NULL,NULL);
+    if(!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
