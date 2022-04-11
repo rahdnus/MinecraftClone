d9d0e74f8f46b38c2388b1312673e0d29b333164
rahdnus <rahdnus119@gmail.com>
2022-04-11 20:31:17 +0530

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ int main()
     glfwMakeContextCurrent(window);
     while(!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
