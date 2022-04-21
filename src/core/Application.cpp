9b58696108d2ca08de1cdde91b0bc011d29a8026
rahdnus <rahdnus119@gmail.com>
2022-04-21 08:54:03 +0530

    Added Sqaure

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -4,12 +4,19 @@
 
 using namespace Core;
 
-    GLuint VAO_ID,VBO_ID;
+    GLuint VAO_ID,VBO_ID,EBO_ID;
 
-Vertex vertex[3]={
-    {glm::vec3(0.5f,-0.5f,0),glm::vec4(0,1,1,1)},
-    {glm::vec3(-0.5f,0.5f,0),glm::vec4(1,0,1,1)},
-    {glm::vec3(-0.5f,-0.5f,0),glm::vec4(1,0,1,1)}
+Vertex vertex[4]={
+    {glm::vec3(0.5f,0.5f,0),glm::vec4(1,0,1,1)},//0
+    {glm::vec3(-0.5f,0.5f,0),glm::vec4(1,0,1,1)},//1
+    {glm::vec3(-0.5f,-0.5f,0),glm::vec4(1,0,1,1)},//2
+    {glm::vec3(0.5f,-0.5f,0),glm::vec4(0,1,1,1)}//3
+
+};
+GLuint indices[]
+{
+    0,1,3,
+    1,2,3
 };
 ShaderProgram program;
 int Application::windowInit(GLuint width,GLuint height,std::string name)
@@ -35,6 +42,9 @@ int Application::windowInit(GLuint width,GLuint height,std::string name)
     glBindBuffer(GL_ARRAY_BUFFER,VBO_ID);
     
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertex),vertex,GL_STATIC_DRAW);
+    glCreateBuffers(1,&EBO_ID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO_ID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
     
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(Vertex),(void*)offsetof(Vertex,position));
     glEnableVertexAttribArray(0);
@@ -56,7 +66,7 @@ int Application::run()
         glClear(GL_COLOR_BUFFER_BIT);
         program.bind();
         glBindVertexArray(VAO_ID);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
 
         glfwSwapBuffers(mywindow);
         glfwPollEvents();
